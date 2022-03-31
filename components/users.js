3fb1e489d8401c33856eccd173caa23807abc809
Carl <carl.scrivener.89@gmail.com>
2022-03-31 17:09:40 +0100

    add ui for loading and error states

--- components/users.js
+++ components/users.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import CircularProgress from '@mui/material/CircularProgress';
+import WarningIcon from '@mui/icons-material/Warning';
 import UserItem from './user-item';
 import PaginationButtons from './pagination-buttons';
 import userActions from '../redux/actions/actions';
@@ -26,6 +28,30 @@ const Users = function Users() {
         Users List
       </Typography>
       <Grid container spacing={3}>
+        {users.loading && (
+          <Grid item xs={12}>
+            <Box mt={5} mb={5}>
+              <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <CircularProgress color="error" />
+              </Box>
+              <Typography variant="h6" align="center" mt={1}>
+                Loading Users...
+              </Typography>
+            </Box>
+          </Grid>
+        )}
+        {users.error && !users.loaded && (
+          <Grid item xs={12}>
+            <Box mt={5} mb={5}>
+              <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <WarningIcon color="error" />
+              </Box>
+              <Typography variant="h6" align="center" mt={1}>
+                Failed to load users.
+              </Typography>
+            </Box>
+          </Grid>
+        )}
         {users.loaded &&
           users.users.data.map((user) => (
             <Grid key={user.id} item xs={12} sm={4}>
